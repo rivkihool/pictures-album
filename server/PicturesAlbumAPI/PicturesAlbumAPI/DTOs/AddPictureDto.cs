5285e15727e41b9e51b328586bd4baf24a823742
Rivka Hool <rivkihool@gmail.com>
2025-07-31 02:49:49 +0300

    changes in server side

--- server/PicturesAlbumAPI/PicturesAlbumAPI/DTOs/AddPictureDto.cs
+++ server/PicturesAlbumAPI/PicturesAlbumAPI/DTOs/AddPictureDto.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
-namespace PicturesAlbumAPI.DTO
+namespace PicturesAlbumAPI.DTOs
 {
     public class AddPictureDto
     {
+        [Required(ErrorMessage = "Picture name is required.")]
+
         public string Name { get; set; } = string.Empty;
         public DateTime? Date { get; set; }
         public string? Description { get; set; }
+        [Required(ErrorMessage = "Picture file is required.")]
+
         public IFormFile File { get; set; } = default!;
     }
 
